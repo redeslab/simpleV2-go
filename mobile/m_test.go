bbadf88302656b01af3bd9b182c727ceff5031ca
hopwesley <ws@sinoc.io>
2022-07-20 16:16:33 +0800

    test case

--- mobile/m_test.go
+++ mobile/m_test.go
@@ -25,3 +25,35 @@ func TestQueryByAddr(t *testing.T) {
 	ret := RefreshHostByAddr(userAddr)
 	fmt.Println("ip:", string(ret))
 }
+
+func TestRuleVerInt(t *testing.T) {
+	ret, err := RuleVerInt()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(ret))
+}
+
+func TestDnsRule(t *testing.T) {
+	ret, err := RuleDataLoad()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(ret)
+}
+
+func TestIpMust(t *testing.T) {
+	ret, err := MustHitData()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(ret)
+}
+
+func TestByPassDataLoad(t *testing.T) {
+	ret, err := ByPassDataLoad()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(ret)
+}
